b45e21399724e79050e4368bf64f62c0e1581d2e
Zac Ambrose <zambrose2017@gmail.com>
2017-02-24 15:29:30 -0700

    edited about and contact routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
 
-  get 'pages/about'
+  
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
-  get 'pages/contact'
-
+  
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ root to:"pages#home"
 end
