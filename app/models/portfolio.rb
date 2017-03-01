ae1350911e5e1f4cbfe59399de96774baa2c5796
Zac Ambrose <zambrose2017@gmail.com>
2017-03-01 08:56:23 -0700

    implemented defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,9 +1,17 @@
 class Portfolio < ApplicationRecord
-validates_presence_of :title, :body, :main_image, :thumb_image 
+	validates_presence_of :title, :body, :main_image, :thumb_image 
 
-def self.angular
-	where(subtitle: "Angular")
+	def self.angular
+		where(subtitle: "Angular")
 	end
-#both of these do the same thing ^^ an the scope below
+	#both of these do the same thing ^^ an the scope below
 	scope :ruby_on_rails_portfolio_items, -> {where(subtitle: "Ruby on Rails")}
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "http://placehold.it/600x400"
+		self.thumb_image ||= "http://placehold.it/350x200"
+	end
 end
+
