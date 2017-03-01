755643fcc48a999a00c8f48e1084d192c11a32c2
Zac Ambrose <zambrose2017@gmail.com>
2017-03-01 08:34:05 -0700

    implemented custome scopes for portfolio

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,18 @@
+3.times do |topic|
+	Topic.create!(
+		title: "Topic #{topic}"
+)
+end
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
   	title:"My Blog Post #{blog}",
-  	body:  "here is the body of my text jsadkfjsdknvjkansjkdfhsjncjkshfjhsjvjansjkdnfjksdvjkajksndjkhfjksd"
-  	)  	end
+  	body:  "here is the body of my text jsadkfjsdknvjkansjkdfhsjncjkshfjhsjvjansjkdnfjksdvjkajksndjkhfjksd",
+
+  	topic_id: Topic.last.id 
+  	)	
+end
 
 puts "10 blog posts created"
 
@@ -15,14 +25,23 @@ puts "10 blog posts created"
 
 	puts "5 skills created"
 
-	9.times do |portfolio_item|
+	8.times do |portfolio_item|
 		Portfolio.create!(
 			title: "Portfolio title #{portfolio_item}",
-			subtitle: "My great Service",
+			subtitle: "Ruby on Rails",
 			body: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
 			main_image: "http://placehold.it/600x400",
 			thumb_image:"http://placehold.it/350x200"
 		)
 	end
 
+1.times do |portfolio_item|
+		Portfolio.create!(
+			title: "Portfolio title #{portfolio_item}",
+			subtitle: "Angular",
+			body: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+			main_image: "http://placehold.it/600x400",
+			thumb_image:"http://placehold.it/350x200"
+		)
+	end
 	puts "9 portfolio items created"
