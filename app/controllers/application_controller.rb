5abf142762b5b4f0bfec956fcc1537ed52855b9e
Zac Ambrose <zambrose2017@gmail.com>
2017-03-02 16:39:37 -0700

    added name feild for used and customized attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+before_filter :configure_permitted_parameters, if: :devise_controller?
+ #configure parameters if devise controller is active or true
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+		#for the devise parameters(email, password.. lets also allow the following keys.. "name" )
+	end
 end
