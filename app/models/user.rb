8f2930aade3065fbc80f4caa7ad493b95bf16e35
Zac Ambrose <zambrose2017@gmail.com>
2017-03-02 17:08:30 -0700

    added user greeting using virtual attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :name
+
+  def first_name
+  	self.name.split.first
+  	#takes in a name and takes the first paramater.. (the first name)
+  end 
+
+  def last_name
+  	self.name.split.last
+  	#takes in a name and takes the second paramater.. (the last name)
+  end
+
 end
