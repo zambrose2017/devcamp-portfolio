df5cee1a74bf648236ae5d13e3c33eb31fbdce27
Zac Ambrose <zambrose2017@gmail.com>
2017-03-06 11:52:30 -0700

    added nav bar and default header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,12 @@
+module DefaultPageContent
+extend ActiveSupport::Concern
+  
+included do
+  before_filter :set_page_defaults
+end
+    def set_page_defaults
+  @page_title = "Devcamp Portfolio | My Portfolio Website"
+  @seo_keywords = "jordan Hudgens porftolio"
+  end
+end
+
