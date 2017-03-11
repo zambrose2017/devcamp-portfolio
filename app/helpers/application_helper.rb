fc9b84dedee025eac91fde1da4cf881c72bd30b0
Zac Ambrose <zambrose2017@gmail.com>
2017-03-11 09:17:49 -0700

    Refactored navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,5 +20,43 @@ def copyright_generator
   ZambroseViewTool::Renderer.copyright "Zac Ambrose", "All rights reserved"
 end
 
+  def nav_items
+  [
+    {
+      url:root_path,
+      title: "Home"
+    },
+    {
+      url:about_path,
+      title: "About me"
+    },
+    {
+      url:contact_path,
+      title: "contact"
+    },
+    {
+      url:blogs_path,
+      title: "Blogs"
+    },
+    {
+      url:portfolios_path,
+      title: "Portfolios"
+    }, 
+
+  ]
+  end  
+
+  def nav_helper css_style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{css_style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path   
+  end
 end
 
