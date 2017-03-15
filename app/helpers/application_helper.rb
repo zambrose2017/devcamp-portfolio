28301d581d4cac0d3bc5b9515b513ee885122571
Zac Ambrose <zambrose2017@gmail.com>
2017-03-15 14:06:53 -0600

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -58,5 +58,17 @@ end
   def active? path
     "active" if current_page? path   
   end
-end
 
+def alerts 
+  alert = (flash[:alert] || flash[:error] || flash[:notice]) 
+  
+  if alert
+    alert_generator alert
+  end
+end 
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Zac", sticky: false )
+  end
+
+end
