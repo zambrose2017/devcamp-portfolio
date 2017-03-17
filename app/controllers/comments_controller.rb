3b2d5e2a0737312f5ec49761f2b30c364323df09
Zac Ambrose <zambrose2017@gmail.com>
2017-03-16 20:53:47 -0600

    build out inital and partial call for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
-end
+  def create
+    @comment= current_user.comments.build(comment_params)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end    
