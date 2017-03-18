8a78ca39c7bdc43ec35ff1fbf4f52cc5405d5c53
Zac Ambrose <zambrose2017@gmail.com>
2017-03-18 09:25:23 -0600

    implemented custome scope for recent blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,9 +5,11 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-
-    @blogs = Blog.page(params[:page]).per(5)
-
+    if logged_in?(:site_admin)
+    @blogs = Blog.recent.page(params[:page]).per(5)
+    else
+      @blogs = Blog.published.recent.page(params[:page]).per(5)
+    end
     @page_title = "My Portfolio Blog"
   end
 
@@ -23,7 +25,7 @@ class BlogsController < ApplicationController
 
   # GET /blogs/new
   def new
-    @blog = Blog.new
+    @blog = Blog.newrecent.
   end
 
   # GET /blogs/1/edit
@@ -89,4 +91,5 @@ class BlogsController < ApplicationController
     def blog_params
       params.require(:blog).permit(:title, :body)
     end
+  end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -12,4 +12,10 @@ class Blog < ApplicationRecord
   def self.special_blogs
     all
   end
+
+  def self.recent
+    order("created_at DESC") 
+  end
 end
+
+
